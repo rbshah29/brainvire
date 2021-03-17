3f31f81773cdb6c739cba3f302b7986d829d8e69
rbshah29 <rbshah0029@gmail.com>
2021-03-17 19:07:53 +0530

    today-17/3/21

--- abcd/HotelManagement/models/hotel_management.py
+++ abcd/HotelManagement/models/hotel_management.py
@@ -14,6 +14,7 @@ class HotelRoom(models.Model):
     room_size = fields.Integer()
     boolean = fields.Boolean()
     room_regi = fields.Many2one('hotel.registration')
+    
     state = fields.Selection([
         ('draft', 'Draft'),
             ('allocated', 'Allocated')],default='draft')
@@ -48,11 +49,14 @@ class HotelRegistration(models.Model):
     mobile = fields.Integer()
     date_of_birth = fields.Date()
 
+    #we can use either this for customer and guest  
+    #1
     room_ids = fields.One2many('customer.guest.line','regi_id')
-
+    #2
     room_regi_ids = fields.Many2one('hotel.room' , domain=[('state','=','draft')])
 
     document = fields.One2many('customer.document','regi',required=True)
+
     guest_list = fields.One2many('customer.guest','registration_guest')
 
     start_date = fields.Date('Start Date')
@@ -67,10 +71,10 @@ class HotelRegistration(models.Model):
                'self.service') or 'New'
 
        # to make state of specific room allocated when save is pressed    
-       val = {'state':'allocated'}
-       room_allocate = self.env['hotel.room'].search([('id', '=', vals['room_regi_ids'])])
-       for i in room_allocate:
-            i.write(val)
+       # val = {'state':'allocated'}
+       # room_allocate = self.env['hotel.room'].search([('id', '=', vals['room_regi_ids'])])
+       # for i in room_allocate:
+       #      i.write(val)
 
        result = super(HotelRegistration, self).create(vals)
        return result
@@ -95,6 +99,7 @@ class CustomerGuest(models.Model):
     _name = 'customer.guest'
 
     name = fields.Char(required=True)
+
     document_guest = fields.One2many('customer.document','guest',required=True)
     registration_guest = fields.Many2one('hotel.registration')
 
@@ -102,7 +107,9 @@ class CustomerGuest(models.Model):
 class CustomerDocument(models.Model):
     _name = "customer.document"
 
+    #m2o created to support o2m
     regi = fields.Many2one('hotel.registration')
+    #m2o created to support o2m
     guest = fields.Many2one('customer.guest')
     document = fields.Binary(required=True)
     document_name = fields.Char(required=True)
@@ -110,12 +117,15 @@ class CustomerDocument(models.Model):
 # table for registration inquiry
 class RegistrationInquiry(models.Model):
     _name = 'registration.inquiry'
-# 
+    
+    # m2ofield connecting res.partner
     customer = fields.Many2one("res.partner",required=True)
     startDate = fields.Date()
     endDate = fields.Date()
+    #m2o field connecting room types
     room_type_id = fields.Many2one('room.type',required=True)
     room_size = fields.Integer(required=True)
+    #o2m connecting hotel room with specific domain
     room_regi = fields.One2many('hotel.room' ,'many_one', domain=[('state','=','draft')])
 
 
@@ -132,19 +142,27 @@ class RegistrationInquiry(models.Model):
         return 
     #function for button
     def inquiry_one(self):
-        print("\n\n\n\n\n\n--------_________-------______-----_____--")
         #to check conditoins 
+
+        #to pass only those element which are selected using many2one and one2many field
+        #this is use to pass only selected rooms in this module
+        res=[]
+        for i in self:
+            for j in i.room_regi:
+                if j.boolean:
+                    res.append({'room_id':j.id})
+
+        #if these condition are true then ir will return anything given             
         inquiry = self.env['hotel.room'].search(['&',('room_type','=',self.room_type_id.id),
             ('room_size','>=',self.room_size),('state','=','draft')]) 
 
         if inquiry:
-            print("---------------->>>>>>>>>>>>----AVAIABLE-------<<<<<<<<<<-------------")
             #if condition is satisfied it will return a specific view
             return {
              'name': 'inquiry_registration_form',
              'res_model': 'hotel.registration',
              'type': 'ir.actions.act_window',
-             'context': {},
+             'context': {'default_room_ids':res},
              'view_mode': 'form',
              'view_type': 'form',
              'view_id': self.env.ref("HotelManagement.regi_form").id,
@@ -155,9 +173,13 @@ class RegistrationInquiry(models.Model):
             print("___--------___________---------____________--------___________----------________---------_______")
             raise ValidationError("Room not AVAIABLE")
 
+
 class GuestLine(models.Model):
     _name = 'customer.guest.line'
 
+    #m2m field connected to customer guest to get list of guest 
     guest_ids = fields.Many2many("customer.guest",required=True)
+    #m2o field created just for o2m in registration field
     regi_id = fields.Many2one('hotel.registration')
+    #m2o field connecting hotel rooms
     room_id = fields.Many2one('hotel.room')
